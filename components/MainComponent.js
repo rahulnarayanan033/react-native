818bb98e4382afd1ccd7627ae3fbe825d6cda3db
rahulnarayanan033 <rahulnarayanan033@gmail.com>
2020-06-22 20:12:53 +0530

    React Native Assignment 1 Solution

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { View , Platform } from 'react-native';
 import { createStackNavigator , createDrawerNavigator } from 'react-navigation';
 import Home from './HomeComponent';
@@ -39,6 +41,38 @@ const HomeNavigator = createStackNavigator({
 }
 );
 
+const AboutNavigator = createStackNavigator({
+    Contact : { screen : About }
+},
+{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+    }
+}
+);
+
+const ContactNavigator = createStackNavigator({
+    Contact : { screen : Contact }
+},
+{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+    }
+}
+);
+
 const MainNavigator = createDrawerNavigator({
     Home : { 
         screen : HomeNavigator,
@@ -47,13 +81,27 @@ const MainNavigator = createDrawerNavigator({
             drawerLabel : 'Home'
         }
      },
+     About : {
+         screen : AboutNavigator,
+         navigationOptions : {
+             title : 'About Us',
+             drawerLabel : 'About Us'
+         }
+     },
      Menu : {
          screen : MenuNavigator,
          navigationOptions : {
              title : 'Menu',
              drawerLabel : 'Menu'
          }
+     },
+     Contact : {
+         screen : ContactNavigator,
+         navigationOptions : {
+             drawerLabel : 'Contact Us'
+         }
      }
+
 },
 {
     drawerBackgroundColor : '#D1C4E9'
